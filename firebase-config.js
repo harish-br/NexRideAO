67f12eac184fddb0e74a45e3e2d0d0f8a61a64a3
harish-br <harishsrhr@gmail.com>
2026-06-24 16:04:32 +0530

    Move Firebase config to environment variables

--- firebase-config.js
+++ firebase-config.js
@@ -5,14 +5,14 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBWS5Ah5TNrdrZFiTPG6WY0bG8c2BvFrb8",
-    authDomain: "nexride-ao.firebaseapp.com",
-    databaseURL: "https://nexride-ao-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "nexride-ao",
-    storageBucket: "nexride-ao.firebasestorage.app",
-    messagingSenderId: "683594734736",
-    appId: "1:683594734736:web:ccbbed5510277bcca58c15",
-    measurementId: "G-S5XY5GEPZE"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if API key is provided (prevents crashing if not setup)
